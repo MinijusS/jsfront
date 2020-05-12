2f4bbe3a2171b5fc194c1c65fee815c5c320e78b
MinijusS <savickasminijus1@gmail.com>
2020-05-12 10:54:48 +0300

    messenger js

--- script.js
+++ script.js
@@ -1,77 +1,36 @@
 'use strict';
 
-const ul = document.querySelector('ul');
-const input = document.querySelector('input');
-const edit = document.querySelector('input[name=edit]');
+const button = document.querySelector('button');
+const chat = document.querySelector('.chat-container');
 
-input.addEventListener('keypress', onEnterKeyPress);
+button.addEventListener('click', () => {
+    const input = document.querySelector('textarea');
 
-function onEnterKeyPress(event) {
-    if(event.keyCode === 13 && event.target.value.trim() != '') {
-        const li = createLiElement(event);
-        const deleteBtn = createButton('Delete');
-        const editBtn = createButton('Edit');
-        let div = document.createElement('div');
+    chat.appendChild(createMessage(input.value));
 
-        div.setAttribute('class', 'buttons');
-        div.appendChild(deleteBtn);
-        div.appendChild(editBtn);
-    
-        li.appendChild(div);
-        ul.appendChild(li);
+    input.value = '';
+});
 
-        deleteBtn.addEventListener('click', onDeleteClick);
-        editBtn.addEventListener('click', editLi);
-        event.target.value = '';
-    }
-}
+function createMessage(text) {
+    const article = document.createElement('article');
+    const image = document.createElement('section');
+    const chatBubble = document.createElement('section');
 
-function onDeleteClick(event) {
-    let closestLi = event.target.closest('li');
-    closestLi.classList.add('deleteTransition');
-    setTimeout(pop, 1000);
+    article.classList.add('message', 'self');
+    chatBubble.classList.add('chat-bubble');
+    image.classList.add('image');
 
-    function pop() {
-        closestLi.remove();
-    }
-}
-
-function createButton(name) {
-    
-    let button = document.createElement('button');
-    let buttonText = document.createTextNode(name);
-    button.setAttribute('class', name);
-    button.appendChild(buttonText);
-
-    return button;
-}
-
-function createLiElement(event) {
-    let li = document.createElement('li');
-    let div = document.createElement('div');
-    let text = document.createTextNode(event.target.value);
-    div.appendChild(text);
-    li.appendChild(div);
-
-    return li;
-}
-
-function editLi(event) {
-    let li = event.target.closest('li').firstChild;
-    
-    edit.style.display = 'block';
-    edit.value = li.textContent;
-    edit.focus();
-
-    function getEditInput(event) {
-        if(event.keyCode == 13 && event.target.value.trim() != ''){
-            li.textContent = event.target.value.trim();
-            edit.value = '';
-            edit.style.display = 'none';
-
-            edit.removeEventListener('keyup', getEditInput);
+    text.split('\n').forEach(para => {
+        if(para != ''){
+            const paragraph = document.createElement('p');
+            const textNode = document.createTextNode(para);
+            paragraph.appendChild(textNode);
+            chatBubble.appendChild(paragraph);
         }
-    }
+    });
 
-    edit.addEventListener('keyup', getEditInput);
+    article.appendChild(image);
+    article.appendChild(chatBubble);
+
+    return article;
 }
